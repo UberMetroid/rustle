8c47060d0cbce807228c5ea63b41fae86130d84c
Jeryd <jeryd@example.com>
2026-03-21 18:18:41 -1000

    feat: improve share flow and increment version to v1.1.0

---
+++ target/release/deps/wordle_server-fe7c0dee2456d2d5.d
@@ -0,0 +1,5 @@
+/home/jeryd/Projects/rustle/target/release/deps/wordle_server-fe7c0dee2456d2d5.d: wordle-server/src/main.rs
+
+/home/jeryd/Projects/rustle/target/release/deps/wordle_server-fe7c0dee2456d2d5: wordle-server/src/main.rs
+
+wordle-server/src/main.rs:
